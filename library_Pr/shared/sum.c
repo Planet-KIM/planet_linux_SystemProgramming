32ff122fbbf8675d597e324e65a99a17feef3e21
KIM-DO-WON <55446103+KIM-DO-WON@users.noreply.github.com>
2020-03-28 14:06:57 +0900

    How to make the libarary file -> it is libarary practice.

---
+++ library_Pr/shared/sum.c
@@ -0,0 +1,7 @@
+int sum (int to){
+	int i, result = 0;
+
+	for(i=0 ; i<=to ; i++)
+		result+=i;
+	return (result);
+}
---
+++ library_Pr/static/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+extern int sum(int);
+
+void main(){
+	printf("%d\n", sum(100));
+}
